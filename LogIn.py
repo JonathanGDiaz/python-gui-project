da888f02a1637dc671ebac67509c522f07ff2e48
JonathanGDiaz <morokeidovah@gmail.com>
2023-05-12 23:03:46 -0600

    Feat: Log in implementation

--- LogIn.py
+++ LogIn.py
@@ -1,5 +1,8 @@
 import tkinter
+from tkinter import messagebox
 from tkinter import ttk as tk
+from PIL import Image, ImageTk
+from Cursor import Cursor
 
 
 class LogIn(tkinter.Frame):
@@ -10,30 +13,52 @@ class LogIn(tkinter.Frame):
         self.emailEntry = customEntry(self, "Email", False)
         self.passwordEntry = customEntry(self, "Password", True)
         self.submitButton = tk.Button(self)
+        self.image = Image.open("assets/logo.png")
+        self.image = self.image.resize((150, 150))
+        self.imagePI = ImageTk.PhotoImage(self.image)
+        self.imageLabel = tk.Label(self)
+        self.cursor = Cursor()
 
         self.widgetsConfig()
 
     def widgetsConfig(self):
-        self.mainLabel.configure(
-            text="User login", font=('forest-dark', 30, 'bold'))
+        self.imageLabel.configure(image=self.imagePI)
 
-        # Email configuration
+        self.mainLabel.configure(
+            text="User login", font=("forest-dark", 30, "bold"))
+
         self.emailEntry.configure(width=25)
 
-        # Password configuration
-        self.passwordEntry.configure(width=25, show="\u2022")
+        self.passwordEntry.configure(width=25)
 
-        # Submit button
         self.submitButton.configure(
-            width=20, text="Log in", command=lambda: self.master.showFrame("Dashboard"))
+            width=20, text="Log in", command=self.logIn)
         self.render()
         return
 
+    def logIn(self):
+        credentials = {
+            "email": self.emailEntry.get(),
+            "password": self.passwordEntry.get()
+        }
+        response = self.cursor.logIn(credentials=credentials)
+        if response:
+            self.emailEntry.delete(0, "end")
+            self.passwordEntry.delete(0, "end")
+            self.emailEntry.insert(0, self.emailEntry.placeHolder)
+            self.passwordEntry.insert(0, self.passwordEntry.placeHolder)
+            self.passwordEntry.configure(show="")
+            self.master.showFrame("Dashboard")
+        else:
+            messagebox.showerror(message="The email or password is wrong")
+        pass
+
     def render(self):
-        self.mainLabel.grid(column=0, row=0, columnspan=2)
-        self.emailEntry.grid(column=1, row=1, pady=10)
-        self.passwordEntry.grid(column=1, row=2, pady=10)
-        self.submitButton.grid(column=0, row=3, columnspan=2, pady=5)
+        self.imageLabel.grid(column=0, row=0, columnspan=2)
+        self.mainLabel.grid(column=0, row=1, columnspan=2)
+        self.emailEntry.grid(column=1, row=2, pady=10)
+        self.passwordEntry.grid(column=1, row=3, pady=10)
+        self.submitButton.grid(column=0, row=4, columnspan=2, pady=5)
         return
 
 
